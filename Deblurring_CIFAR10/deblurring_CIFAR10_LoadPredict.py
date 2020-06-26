6501a15d98de4f3cd625e9a46fec39ad645af17e
FrancescoBonzi <fra.bonzi@gmail.com>
2020-06-26 15:23:47 +0200

    refactor name LoadPredict

--- Deblurring_CIFAR10/deblurring_CIFAR10_LoadPredict.py
+++ Deblurring_CIFAR10/deblurring_CIFAR10_LoadPredict.py
@@ -10,13 +10,14 @@ from utilities import SSIMLoss, PSNR, build_dataset, print_dataset, inspect_repo
 tf.keras.backend.set_floatx('float64')
 width = 32
 height = 32
+name_model = "SkipConnections"
 
 
 ######################################
 ### LOAD THE MODEL AND THE DATASET ###
 ######################################
 
-model = tf.keras.models.load_model("./models/SkipConnections", custom_objects={'SSIMLoss': SSIMLoss, 'PSNR': PSNR})
+model = tf.keras.models.load_model("./models/"+name_model, custom_objects={'SSIMLoss': SSIMLoss, 'PSNR': PSNR})
 (_, _), (test_images, _) = datasets.cifar10.load_data()
 test_images =  test_images / 255.0 # Normalize pixel values to be between 0 and 1
 test_images = test_images[10:20, :, :, :]
