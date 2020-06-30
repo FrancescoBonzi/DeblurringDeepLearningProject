e6daff4f0eb58a09ba81538cdd6c6a4ca7ec0708
Utente <utente@iMac-Bonzis.homenet.telecomitalia.it>
2020-06-30 12:03:55 +0200

    comment line for reduce training set

--- Deblurring_CIFAR10/deblurring_CIFAR10_ResNet_FitSave.py
+++ Deblurring_CIFAR10/deblurring_CIFAR10_ResNet_FitSave.py
@@ -23,7 +23,7 @@ EPOCHS = 35
 (train_images, _), (_, _) = datasets.cifar10.load_data()
 ''' Normalize pixel values to be between 0 and 1 '''
 train_images = train_images / 255.0
-train_images = train_images[1:1000,:,:,:]
+#train_images = train_images[1:1000,:,:,:]
 
 train_blurred_images, train_rands = build_dataset(train_images)
 
