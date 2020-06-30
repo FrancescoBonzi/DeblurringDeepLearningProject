ecedd29d4bb4198ff50fb239e8c15b3a380b09b6
Utente <utente@iMac-Bonzis.homenet.telecomitalia.it>
2020-06-30 21:07:22 +0200

    ready for training SkipConnections

--- Deblurring_CIFAR10/deblurring_CIFAR10_SkipConnections_FitSave.py
+++ Deblurring_CIFAR10/deblurring_CIFAR10_SkipConnections_FitSave.py
@@ -5,6 +5,7 @@ from tensorflow.keras.layers import Conv2D, Conv2DTranspose
 import numpy as np
 import os
 import dill
+import time
 
 from utilities import SSIMLoss, PSNR, build_dataset, print_dataset, extract_from_report
 from autoencoder_models import DeblurringSkipConnections
@@ -24,7 +25,7 @@ EPOCHS = 35
 (train_images, _), (_, _) = datasets.cifar10.load_data()
 ''' Normalize pixel values to be between 0 and 1 '''
 train_images = train_images / 255.0
-#train_images = train_images[1:1000, :, :, :]
+#train_images = train_images[1:100, :, :, :]
 
 train_blurred_images, train_rands = build_dataset(train_images)
 
@@ -47,13 +48,15 @@ early_stop = tf.keras.callbacks.EarlyStopping(
 
 model.compile(loss=SSIMLoss, optimizer=tf.keras.optimizers.Adam(),
               metrics=['mse', 'mae', PSNR])
+start = time.time()              
 report = model.fit(x=train_blurred_images,
                    y=train_images,
                    batch_size=32,
                    epochs=EPOCHS,
                    callbacks=[early_stop],
                    validation_split=0.25)
-
+end = time.time()
+print("Time: ", end - start, "s")
 
 #################################
 #### SAVING REPORTS AND MODEL ###
