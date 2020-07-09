602bc0256d4024374ae1b09c26d4cdcbcf75d369
Utente <utente@iMac-Bonzis.homenet.telecomitalia.it>
2020-07-09 16:15:25 +0200

    upload ResNet_v1 training

--- deblurring_CIFAR10_configuration.py
+++ deblurring_CIFAR10_configuration.py
@@ -2,10 +2,10 @@ import tensorflow as tf
 from utilities import get_metrics, get_other_metrics, get_model, get_loss, SSIMLoss, PSNR
 tf.keras.backend.set_floatx('float64')
 
-demo = True
-model_name = 'CNNBase_v1'
-loss_name = 'mse' # mse, mae, SSIMLoss, PSNR
-EPOCHS = 1
+demo = False
+model_name = 'ResNet_v1'
+loss_name = 'SSIMLoss' # mse, mae, SSIMLoss, PSNR
+EPOCHS = 36
 
 test_lower_bound = 10 #parameters to limit the test prediction on a subset of the dataset
 test_upper_bound = 20
