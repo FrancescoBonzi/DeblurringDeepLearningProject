613f20d63f217bed71bec7d59f1f1e16b6b5548a
AndreaZanellati87 <andrea.zanellati@gmail.com>
2020-07-11 14:56:14 +0200

    failed debug

--- kernel_motion_estimator.py
+++ kernel_motion_estimator.py
@@ -11,13 +11,16 @@ import os
 import random
 import cv2
 from math import sin, pi
-from utilities import print_dataset, rebuild_images, load_REDs, get_frames_per_video, get_num_videos
+from utilities import print_dataset, rebuild_images, load_REDs, get_frames_per_video, get_num_videos, split_REDs
 
 num_patches_width = 16 
-num_patches_height = 6 
-width = 320
-height = 180
+num_patches_height = 9 
+original_width = 320
+original_height = 180
+width = int(original_width/num_patches_width)       #patches dimensions without considering overlapping
+height = int(original_height/num_patches_height)
 motion_kernel_size = 20
+patches_size = 20
 
 test_directory = "./datasetREDs/test_blur"
 test_num_videos = len(os.listdir(test_directory))
@@ -74,7 +77,7 @@ def motion_kernel_generator(angle, length):
 
 # It builds the REDs dataset of motion blurred patches
 # We suppose .DS_Store is not in the directories
-def build_dataset_for_motion_blur(directory, num_patches=20, dim_patches=20):
+def build_dataset_for_motion_blur(directory, num_patches=20, dim_patches= patches_size):
     dataset = []
     labels = []
     num_videos = len(os.listdir(directory))
@@ -101,7 +104,7 @@ def build_dataset_for_motion_blur(directory, num_patches=20, dim_patches=20):
 def convert_to_motion_vector(label):
     count = 0
     for orientation in range(6):
-        for length in range(1, 26, 2):
+        for length in range(1, motion_kernel_size+1, 2):
             if orientation != 0 and length == 1: break
             if count == label:
                 return (length, orientation)
@@ -111,7 +114,7 @@ def convert_to_motion_vector(label):
 def convert_to_label(l, o):
     count = 0
     for orientation in range(6):
-        for length in range(1, 26, 2):
+        for length in range(1, motion_kernel_size+1, 2):
             if orientation != 0 and length == 1: break
             if o == orientation and l == length:
                 return count
@@ -125,12 +128,12 @@ def rotate_image(image, angle = 6, num_rotation = 6):
         rotated_images.append(rotated_image)
         #plt.imshow(rotated_image)
         #plt.show()
-    return rotated_images
+    return np.array(rotated_images)
 
 
-def get_motion_vector_prediction(image):
+def get_motion_vector_prediction(model, image):
     rotated_images = rotate_image(image)
-    predictions = motion_kernel_estimator.predict(rotated_images) 
+    predictions = model.predict(rotated_images) 
     #expected for each images a vector of 73 elements containing the probability associated to each label
 
     max_probabilities_per_prediction = []
@@ -146,12 +149,12 @@ def get_motion_vector_prediction(image):
 
     return length, selected_motion_orientation
 
-def motion_field_predictor(images):
+def motion_field_predictor(model, images):
     patches = split_REDs(images, test_num_videos, test_frames_per_video, num_patches_width, num_patches_height, height, width, 0)
     predicted_motion_vectors = []
     predicted_motion_kernels = []
     for patch in patches:
-        predicted_length, predicted_orientation = get_motion_vector_prediction(patch)
+        predicted_length, predicted_orientation = get_motion_vector_prediction(model, patch)
         predicted_motion_vectors.append((predicted_length, predicted_orientation))
         predicted_motion_kernels.append(motion_kernel_generator(predicted_length, predicted_orientation))
     return predicted_motion_kernels, predicted_motion_vectors
@@ -175,7 +178,7 @@ print(train_labels.shape)
 batch_size = 32
 
 model = KernelMotionEstimator()
-model.build(input_shape=(batch_size, 30, 30, 3))
+model.build(input_shape=(batch_size, patches_size, patches_size, 3))
 model.summary()
 model.compile(optimizer='adam',
               loss=tf.keras.losses.SparseCategoricalCrossentropy(),
@@ -186,7 +189,7 @@ history = model.fit(train_frames, train_labels, batch_size=batch_size, epochs=2,
 ### TEST ###
 
 test_blurred_REDs = load_REDs("./datasetREDs/test_blur", test_num_videos, test_frames_per_video, original_height, original_width)
-predicted_motion_kernels, predicted_motion_vectors = motion_field_predictor(test_blurred_REDs)
+predicted_motion_kernels, predicted_motion_vectors = motion_field_predictor(model, test_blurred_REDs)
 motion_fields = rebuild_images(predicted_motion_kernels, num_patches_height, num_patches_width, original_width, original_height, height, width, 0)
 print_dataset(test_blurred_REDs, motion_fields)
 
