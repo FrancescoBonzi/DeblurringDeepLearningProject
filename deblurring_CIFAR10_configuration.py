b189ac524dc75d6474465ce8cd52a998a77c8a7a
Utente <utente@iMac-Bonzis.homenet.telecomitalia.it>
2020-07-29 12:58:58 +0200

    upload training 50 epochs SkipConnections_v2 mse

--- deblurring_CIFAR10_configuration.py
+++ deblurring_CIFAR10_configuration.py
@@ -3,7 +3,7 @@ from utilities import get_metrics, get_other_metrics, get_model, get_loss, SSIML
 tf.keras.backend.set_floatx('float64')
 
 demo = False
-model_name = 'SkipConnections_v2'
+model_name = 'ResNet_v2'
 loss_name = 'mse' # mse, mae, SSIMLoss, PSNR
 EPOCHS = 50
 other_EPOCHS = 15
